2817c91104b61fb942c4e5c47caa62ab730b354c
Jairo Júnior <jjdev@Jairos-MacBook-Pro.local>
2022-10-27 17:46:59 -0300

    isIsomorphic

---
+++ isIsomorphic/isIsomorphic.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func isIsomorphic(s string, t string) bool {
+	m := make(map[byte]byte)
+	m2 := make(map[byte]byte)
+
+	for i := 0; i < len(s); i++ {
+		if _, ok := m[s[i]]; !ok {
+			m[s[i]] = t[i]
+		}
+		if _, ok := m2[t[i]]; !ok {
+			m2[t[i]] = s[i]
+		}
+
+		if m[s[i]] != t[i] || m2[t[i]] != s[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println(isIsomorphic("paper", "title")) // truer
+	fmt.Println(isIsomorphic("egg", "add"))     // true
+	fmt.Println(isIsomorphic("foo", "bar"))     // false
+	fmt.Println(isIsomorphic("abc", "abc"))     // true
+	fmt.Println(isIsomorphic("badc", "baba"))   // false
+	fmt.Println(isIsomorphic("egcd", "adfd"))   // false
+}
